22c0336e3943239f4b28a0228eb327d208ad80f8
Entroper <entroper@gmail.com>
2012-12-14 01:02:23 -0500

    content types

--- ChatServer/StaticContentServer.cs
+++ ChatServer/StaticContentServer.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Web;
 using WebSocketListener;
@@ -18,7 +20,7 @@ namespace ChatServer
 				FileStream fs = new FileStream(localPath, FileMode.Open, FileAccess.Read, FileShare.Read);
 
 				context.Response.ContentLength64 = fs.Length;
-				context.Response.ContentType = "text/html";
+				context.Response.ContentType = GetContentType(localPath);
 
 				while(fs.Position != fs.Length)
 				{
@@ -28,6 +30,34 @@ namespace ChatServer
 
 				context.Response.Close();
 			}
+		}
+
+		static Dictionary<string, string> mimeTypes;
+
+		static StaticContentServer()
+		{
+			mimeTypes = new Dictionary<string, string>();
+			mimeTypes["htm"] = "text/html";
+			mimeTypes["html"] = "text/html";
+			mimeTypes["css"] = "text/css";
+			mimeTypes["js"] = "application/javascript";
+			mimeTypes["jpg"] = "text/html";
+			mimeTypes["jpeg"] = "text/html";
+			mimeTypes["gif"] = "text/html";
+			mimeTypes["png"] = "text/html";
+		}
+
+		private static string GetContentType(string localPath)
+		{
+			int suffixPos = localPath.LastIndexOf('.');
+			if (suffixPos == -1)
+				return "text/plain";
+
+			string suffix = localPath.Substring(suffixPos + 1);
+			if (mimeTypes.ContainsKey(suffix))
+				return mimeTypes[suffix];
+			else
+				throw new NotImplementedException("File extension \"." + suffix + "\" is not implemented");
 		}
 	}
 }
